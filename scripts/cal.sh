e7817b7927fe18c9b9812e4b04ede791b8a916e1
Josh Newton <josh.newton@me.com>
2023-12-19 22:11:08 -0500

    fix time display

--- scripts/cal.sh
+++ scripts/cal.sh
@@ -37,7 +37,7 @@ get_next_meeting() {
     --propertyOrder "datetime,title" \
     --noCalendarNames \
     --dateFormat "%A" \
-    --timeFormat "%H:%M" \
+    --timeFormat "%I:%M%p" \
     --includeOnlyEventsFromNowOn \
     --limitItems 1 \
     --excludeAllDayEvents \
@@ -77,20 +77,48 @@ parse_result() {
   # echo $title
 
   # Debug output
-  # echo "Debug: Time variable is set to '$time'"
+  echo "Debug: Time variable is set to '$time'"
+}
+convert_to_24hr_format() {
+  local input_time="$1"
+  # echo "Attempting to convert: '$input_time'" # Debugging line
+
+  # Explicitly set the locale to C to standardize the date format
+  date -j -f "%I:%M %p" "$input_time" +"%H:%M" 2>/dev/null
 }
 
 calculate_times() {
-  # Check if time is in the correct format
-  if [[ $time =~ ^[0-9]{2}:[0-9]{2}$ ]]; then
-    epoc_meeting=$(date -j -f "%T" "$time:00" +%s)
-    epoc_now=$(date +%s)
-    epoc_diff=$((epoc_meeting - epoc_now))
-    minutes_till_meeting=$((epoc_diff / 60))
-  else
-    return # Exit from the function with an error
+  # Convert to 24-hour format and check for errors
+  converted_time=$(convert_to_24hr_format "$time")
+  if [ $? -ne 0 ] || [ -z "$converted_time" ]; then
+    echo "Error in time conversion."
+    return 1
   fi
+
+  echo "Converted time: $converted_time" # Debugging line
+
+  # Now proceed with the date command using the converted time
+  epoc_meeting=$(date -j -f "%H:%M" "$converted_time" +%s 2>/dev/null)
+  if [ $? -ne 0 ]; then
+    echo "Error in date calculation."
+    return 1
+  fi
+
+  epoc_now=$(date +%s)
+  epoc_diff=$((epoc_meeting - epoc_now))
+  minutes_till_meeting=$((epoc_diff / 60))
 }
+# calculate_times() {
+#   # Check if time is in the correct format
+#   if [[ $time =~ ^[0-9]{2}:[0-9]{2}$ ]]; then
+#     epoc_meeting=$(date -j -f "%T" "$time:00" +%s)
+#     epoc_now=$(date +%s)
+#     epoc_diff=$((epoc_meeting - epoc_now))
+#     minutes_till_meeting=$((epoc_diff / 60))
+#   else
+#     return # Exit from the function with an error
+#   fi
+# }
 
 display_popup() {
   tmux display-popup \
