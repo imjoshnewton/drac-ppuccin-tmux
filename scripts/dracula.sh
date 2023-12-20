b3e9f093246f9298dee37a617af8932cea3abf66
Josh Newton <josh.newton@me.com>
2023-12-19 22:29:16 -0500

    Remove extra spaces in time

--- scripts/dracula.sh
+++ scripts/dracula.sh
@@ -271,13 +271,13 @@ main() {
         script=${time_format}
       else
         if $show_day_month && $show_military; then # military time and dd/mm
-          script="%a %d/%m %R ${timezone} "
+          script="%a %d/%m %R ${timezone}"
         elif $show_military; then # only military time
-          script="%a %m/%d %R ${timezone} "
+          script="%a %m/%d %R ${timezone}"
         elif $show_day_month; then # only dd/mm
-          script="%a %d/%m %I:%M %p ${timezone} "
+          script="%a %d/%m %I:%M %p ${timezone}"
         else
-          script="%a %m/%d %I:%M %p ${timezone} "
+          script="%a %m/%d %I:%M %p ${timezone}"
         fi
       fi
     elif [ $plugin = "synchronize-panes" ]; then
